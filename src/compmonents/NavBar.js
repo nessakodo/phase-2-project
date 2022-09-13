ba0942b1106c6b7356f355be0b2e4011f0a45e63
Nessa <nessakodo@gmail.com>
2022-09-13 10:18:09 -0600

    updated broken module

--- src/compmonents/NavBar.js
+++ src/compmonents/NavBar.js
@@ -3,46 +3,31 @@ import HomePage from "./HomePage";
 import ProfilePage from "./ProfilePage";
 import AboutPage from "./AboutPage";
 
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-
-
 export default function NavBar() {
-    // function navigate(e) {
-    //   // don't make a GET request
-    //   e.preventDefault();
-    //   // use pushState to navigate using the href attribute of the <a> tag
-    //   window.history.pushState(null, "", e.target.href);
-   
-  return (
-    <BrowserRouter>
+    function navigate(e) {
+      // don't make a GET request
+      e.preventDefault();
+      // use pushState to navigate using the href attribute of the <a> tag
+      window.history.pushState(null, "", e.target.href);
+    }
+  
+    return (
       <nav className="navbar">
-        <a href="/home" Route path="/home" exact component={HomePage}> Home </a>
-        <a href="/profile" Route path="/profile" exact component={ProfilePage}> Profile </a>
-        <a href="/about" Route path="/about" exact component={AboutPage}> About </a>
-        </nav>
-        </BrowserRouter>
-);
-}
-
-{/*   
-  //   return (
-  //     <nav className="navbar">
-  //       <a href="/home" onClick={navigate}>
-  //         Home
-  //         <HomePage />
-  //       </a>
-  //       <a href="/profile" onClick={navigate}>
-  //         Profile
-  //         <ProfilePage />
-  //       </a>
-  //       <a href="/about" onClick={navigate}>
-  //         About
-  //         <AboutPage />
-  //       </a>
-  //     </nav>
-  //   );
-  // }
-
+        <a href="/home" onClick={navigate}>
+          Home
+          <HomePage />
+        </a>
+        <a href="/profile" onClick={navigate}>
+          Profile
+          <ProfilePage />
+        </a>
+        <a href="/about" onClick={navigate}>
+          About
+          <AboutPage />
+        </a>
+      </nav>
+    );
+  }
 
 
 // import React from "react";
@@ -56,4 +41,4 @@ export default function NavBar() {
 //             <NavLink to="/races">Races</NavLink>
 //         </nav>
 //     );
-// } */}
+// }
