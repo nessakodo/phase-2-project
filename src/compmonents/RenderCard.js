b4600965ea0d3ed170dbf44c4ee4fe0c5e1ba0c3
Nessa <nessakodo@gmail.com>
2022-09-13 16:13:11 -0600

    updated RenderCard

--- src/compmonents/RenderCard.js
+++ src/compmonents/RenderCard.js
@@ -1,61 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 import ClothingLogic from "./ClothingLogic";
 
 
 
 export default function RenderCard( {weather} ) {
+   const [isClicked, setIsClicked] = useState(false)
    if (weather.currentConditions !== undefined) {
-      console.log(weather)
-
    const comment = weather.currentConditions.comment
    const dayhour = weather.currentConditions.dayhour
    const humidity = weather.currentConditions.humidity
    const precip = weather.currentConditions.precip
    const temp = parseInt(weather.currentConditions["temp"]["f"])
    const wind = weather.currentConditions["wind"]["mile"]
-
-console.log(temp)
-
-      const weatherCard = (
-
-
-      <div>
-         <h2>City: {weather.region}</h2>
-            <img 
-               src={weather.currentConditions.iconURL} 
-               alt={weather.currentConditions.comment} 
-            />
-            
-            <p>comment: {comment} </p>
-            <p>dayhour: {dayhour} </p>
-            <p>humidity: {humidity}</p>
-            <p>precip: {precip}</p>
-            
-            <p>temp: {temp}°F</p>
-            <p>wind: {wind}mph </p>
-
+      const weatherCardFront = (
+         <div>
+            <h2>City: {weather.region}</h2>
+               <img
+                  src={weather.currentConditions.iconURL}
+                  alt={weather.currentConditions.comment}
+               />
+               <p>{comment} </p>
+               <p>{dayhour} </p>
+               <p>temp: {temp}°F</p>
+            </div>
+      )
+      const weatherCardBack = (
+         <div>
+               <h4>Clothing Suggestion:</h4>
+               <ClothingLogic
+                  weather={weather}
+               />
+               <p>temp: {temp}°F</p>
+               <p>humidity: {humidity}</p>
+               <p>precip: {precip}</p>
+               <p>wind: {wind}mph </p>
          </div>
       )
-
       return (
-         <div>
-            {weatherCard}
-            <h4>Clothing Suggestion:</h4>
-            <ClothingLogic 
-            weather={weather}
-            />
-   
+         <div onClick={() => setIsClicked(!isClicked)}>
+            {isClicked ? weatherCardBack : weatherCardFront}
          </div>
     );
    }
-   
-   
    return (
       <div>
          <p>loading...</p>
       </div>
    );
 }
+
+
+
+
+
+
+
+
 
 
 
