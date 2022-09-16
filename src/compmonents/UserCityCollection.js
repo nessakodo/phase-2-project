456182396d65abb0e815ef6a5a3daf4ff3fc0ff4
Shelby Lee <slee337@uw.edu>
2022-09-16 09:42:11 -0600

    deleted search bar experiment

--- src/compmonents/UserCityCollection.js
+++ src/compmonents/UserCityCollection.js
@@ -1,19 +1,12 @@
 import React, {useState, useEffect} from "react";
 import RenderCard from "./RenderCard";
 import {v4 as uuid} from "uuid";
-// import SearchBar from "./SearchBar";
 
-// renders all of the city cards of those cities
-// has search bar
 
 export default function UserCityCollection({cityList }) {
 
     // wx data for all searched locations
     const [ allWxData, setAllWxData ] = useState([])
-
-    // // search term state stuff
-    // const [searchTerm, setSearchTerm] = useState("")
-    // const [searchedCities, setSearchedCities] = useState(cityList)
 
     if (cityList) {
         cityList.map((eachCity) => {
@@ -38,11 +31,6 @@ export default function UserCityCollection({cityList }) {
             :
             <p>head over to the profile page to add cities to your account!</p>
             }
-            {/* <div>
-                <SearchBar
-                    onSearchDrama={(userSearchInput) => setSearchTerm(userSearchInput)}
-                /> 
-            </div>   */}
         </div>
     )
 }
